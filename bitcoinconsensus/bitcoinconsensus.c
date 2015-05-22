b2a5d8a635f8b898fdacf7b23e1f2d72b7823d5d
Thomas Kerin <thomas@bitwasp.co>
2015-05-22 22:53:37 +0100

    remove the boilerplate

--- bitcoinconsensus/bitcoinconsensus.c
+++ bitcoinconsensus/bitcoinconsensus.c
@@ -8,10 +8,6 @@
 #include "ext/standard/info.h"
 #include "php_bitcoinconsensus.h"
 
-/* If you declare any globals in php_bitcoinconsensus.h uncomment this:
-ZEND_DECLARE_MODULE_GLOBALS(bitcoinconsensus)
-*/
-
 ZEND_BEGIN_ARG_INFO(arginfo_bitcoinconsensus_verify_script, 0)
     ZEND_ARG_INFO(0, scriptPubKey)
     ZEND_ARG_INFO(0, transaction)
@@ -20,29 +16,17 @@ ZEND_BEGIN_ARG_INFO(arginfo_bitcoinconsensus_verify_script, 0)
     ZEND_ARG_INFO(1, errorFlag)
 ZEND_END_ARG_INFO();
 
-/* True global resources - no need for thread safety here */
-static int le_bitcoinconsensus;
-
-/* {{{ PHP_INI
- */
-/* Remove comments and fill if you need to have entries in php.ini
-PHP_INI_BEGIN()
-    STD_PHP_INI_ENTRY("bitcoinconsensus.global_value",      "42", PHP_INI_ALL, OnUpdateLong, global_value, zend_bitcoinconsensus_globals, bitcoinconsensus_globals)
-    STD_PHP_INI_ENTRY("bitcoinconsensus.global_string", "foobar", PHP_INI_ALL, OnUpdateString, global_string, zend_bitcoinconsensus_globals, bitcoinconsensus_globals)
-PHP_INI_END()
-*/
-/* }}} */
-
-/* Remove the following function when you have successfully modified config.m4
-   so that your module can be compiled into PHP, it exists only for testing
-   purposes. */
-
+/* {{{ proto int bitcoinconsensus_version);
+ * Return the version of libbitcoinconsensus */
 PHP_FUNCTION(bitcoinconsensus_version)
 {
     unsigned int version = bitcoinconsensus_version();
     RETURN_LONG(version);
 }
+/* }}} */
 
+/* {{{ proto int bitcoinconsensus_verify_script(string scriptPubKey, string transaction, int nInput, int flags, int error);
+ * Return true or false for a given scriptPubKey / transaction / nInput */
 PHP_FUNCTION(bitcoinconsensus_verify_script)
 {
     unsigned char *scriptPubKey, *transaction;
@@ -54,47 +38,11 @@ PHP_FUNCTION(bitcoinconsensus_verify_script)
         return;
     }
 
-    int result = bitcoinconsensus_verify_script(scriptPubKey, scriptPubKeyLen, transaction, transactionLen, nInput, flags, &error);
+    int result = bitcoinconsensus_verify_script(scriptPubKey, scriptPubKeyLen, transaction, transactionLen, nInput, flags, error);
     RETURN_LONG(result);
 }
-
-/* Every user-visible function in PHP should document itself in the source */
-/* {{{ proto string confirm_bitcoinconsensus_compiled(string arg)
-   Return a string to confirm that the module is compiled in */
-PHP_FUNCTION(confirm_bitcoinconsensus_compiled)
-{
-	char *arg = NULL;
-	int arg_len, len;
-	char *strg;
-
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &arg, &arg_len) == FAILURE) {
-		return;
-	}
-
-	len = spprintf(&strg, 0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "bitcoinconsensus", arg);
-	RETURN_STRINGL(strg, len, 0);
-}
-/* }}} */
-/* The previous line is meant for vim and emacs, so it can correctly fold and 
-   unfold functions in source code. See the corresponding marks just before 
-   function definition, where the functions purpose is also documented. Please 
-   follow this convention for the convenience of others editing your code.
-*/
-
-
-/* {{{ php_bitcoinconsensus_init_globals
- */
-/* Uncomment this function if you have INI entries
-static void php_bitcoinconsensus_init_globals(zend_bitcoinconsensus_globals *bitcoinconsensus_globals)
-{
-	bitcoinconsensus_globals->global_value = 0;
-	bitcoinconsensus_globals->global_string = NULL;
-}
-*/
 /* }}} */
 
-/* {{{ PHP_MINIT_FUNCTION
- */
 PHP_MINIT_FUNCTION(bitcoinconsensus)
 {
     REGISTER_LONG_CONSTANT("BITCOINCONSENSUS_SCRIPT_FLAGS_VERIFY_NONE", 9, CONST_CS | CONST_PERSISTENT);
@@ -107,93 +55,40 @@ PHP_MINIT_FUNCTION(bitcoinconsensus)
     REGISTER_LONG_CONSTANT("BITCOINCONSENSUS_SCRIPT_FLAGS_VERIFY_MINIMALDATA", (1U << 6), CONST_CS | CONST_PERSISTENT);
     REGISTER_LONG_CONSTANT("BITCOINCONSENSUS_SCRIPT_FLAGS_VERIFY_DISCOURAGE_UPGRADABLE_NOPS", (1U << 7), CONST_CS | CONST_PERSISTENT);
     REGISTER_LONG_CONSTANT("BITCOINCONSENSUS_SCRIPT_FLAGS_VERIFY_CLEANSTACK", (1U << 8), CONST_CS | CONST_PERSISTENT);
-	/* If you have INI entries, uncomment these lines 
-	REGISTER_INI_ENTRIES();
-	*/
 	return SUCCESS;
 }
-/* }}} */
 
-/* {{{ PHP_MSHUTDOWN_FUNCTION
- */
 PHP_MSHUTDOWN_FUNCTION(bitcoinconsensus)
 {
-	/* uncomment this line if you have INI entries
-	UNREGISTER_INI_ENTRIES();
-	*/
 	return SUCCESS;
 }
-/* }}} */
 
-/* Remove if there's nothing to do at request start */
-/* {{{ PHP_RINIT_FUNCTION
- */
-PHP_RINIT_FUNCTION(bitcoinconsensus)
-{
-	return SUCCESS;
-}
-/* }}} */
-
-/* Remove if there's nothing to do at request end */
-/* {{{ PHP_RSHUTDOWN_FUNCTION
- */
-PHP_RSHUTDOWN_FUNCTION(bitcoinconsensus)
-{
-	return SUCCESS;
-}
-/* }}} */
-
-/* {{{ PHP_MINFO_FUNCTION
- */
 PHP_MINFO_FUNCTION(bitcoinconsensus)
 {
 	php_info_print_table_start();
 	php_info_print_table_header(2, "bitcoinconsensus support", "enabled");
 	php_info_print_table_end();
-
-	/* Remove comments if you have entries in php.ini
-	DISPLAY_INI_ENTRIES();
-	*/
 }
-/* }}} */
 
-/* {{{ bitcoinconsensus_functions[]
- *
- * Every user visible function must have an entry in bitcoinconsensus_functions[].
- */
 const zend_function_entry bitcoinconsensus_functions[] = {
-        PHP_FE(bitcoinconsensus_version, NULL)
-        PHP_FE(bitcoinconsensus_verify_script, NULL)
-	PHP_FE(confirm_bitcoinconsensus_compiled,	NULL)		/* For testing, remove later. */
+    PHP_FE(bitcoinconsensus_version, NULL)
+    PHP_FE(bitcoinconsensus_verify_script, arginfo_bitcoinconsensus_verify_script)
 	PHP_FE_END	/* Must be the last line in bitcoinconsensus_functions[] */
 };
-/* }}} */
 
-/* {{{ bitcoinconsensus_module_entry
- */
 zend_module_entry bitcoinconsensus_module_entry = {
 	STANDARD_MODULE_HEADER,
 	"bitcoinconsensus",
 	bitcoinconsensus_functions,
 	PHP_MINIT(bitcoinconsensus),
 	PHP_MSHUTDOWN(bitcoinconsensus),
-	PHP_RINIT(bitcoinconsensus),		/* Replace with NULL if there's nothing to do at request start */
-	PHP_RSHUTDOWN(bitcoinconsensus),	/* Replace with NULL if there's nothing to do at request end */
+	NULL,
+	NULL,
 	PHP_MINFO(bitcoinconsensus),
 	PHP_BITCOINCONSENSUS_VERSION,
 	STANDARD_MODULE_PROPERTIES
 };
-/* }}} */
 
 #ifdef COMPILE_DL_BITCOINCONSENSUS
 ZEND_GET_MODULE(bitcoinconsensus)
 #endif
-
-/*
- * Local variables:
- * tab-width: 4
- * c-basic-offset: 4
- * End:
- * vim600: noet sw=4 ts=4 fdm=marker
- * vim<600: noet sw=4 ts=4
- */
